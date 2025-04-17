e1f2c0b888a150b971839b9beb5be8d8b81c5fc5
Roland Gritzer <gritzer.roland@gmail.com>
2025-04-18 01:31:15 +0200

    umzug

---
+++ MettlerToledoLoadCellTool/MettlerScaleReader.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace MettlerToledoLoadCellTool
+{
+
+    partial class MettlerScaleReader
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.eventBox = new System.Windows.Forms.ListBox();
+            this.loadcellGroupBox = new System.Windows.Forms.GroupBox();
+            this.closeScaleBtn = new System.Windows.Forms.Button();
+            this.tarraScaleBtn = new System.Windows.Forms.Button();
+            this.nullScaleBtn = new System.Windows.Forms.Button();
+            this.openScaleBtn = new System.Windows.Forms.Button();
+            this.tarraWeightLabel = new System.Windows.Forms.Label();
+            this.weightLabel = new System.Windows.Forms.Label();
+            this.printerGroupBox = new System.Windows.Forms.GroupBox();
+            this.feedLabelBtn = new System.Windows.Forms.Button();
+            this.printTestLabelBtn = new System.Windows.Forms.Button();
+            this.closePrinterBtn = new System.Windows.Forms.Button();
+            this.openPrinterBtn = new System.Windows.Forms.Button();
+            this.loadcellGroupBox.SuspendLayout();
+            this.printerGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // eventBox
+            // 
+            this.eventBox.FormattingEnabled = true;
+            this.eventBox.Location = new System.Drawing.Point(8, 120);
+            this.eventBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.eventBox.Name = "eventBox";
+            this.eventBox.Size = new System.Drawing.Size(839, 498);
+            this.eventBox.TabIndex = 0;
+            // 
+            // loadcellGroupBox
+            // 
+            this.loadcellGroupBox.Controls.Add(this.closeScaleBtn);
+            this.loadcellGroupBox.Controls.Add(this.tarraScaleBtn);
+            this.loadcellGroupBox.Controls.Add(this.nullScaleBtn);
+            this.loadcellGroupBox.Controls.Add(this.openScaleBtn);
+            this.loadcellGroupBox.Controls.Add(this.tarraWeightLabel);
+            this.loadcellGroupBox.Controls.Add(this.weightLabel);
+            this.loadcellGroupBox.Location = new System.Drawing.Point(393, 8);
+            this.loadcellGroupBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.loadcellGroupBox.Name = "loadcellGroupBox";
+            this.loadcellGroupBox.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.loadcellGroupBox.Size = new System.Drawing.Size(453, 107);
+            this.loadcellGroupBox.TabIndex = 20;
+            this.loadcellGroupBox.TabStop = false;
+            this.loadcellGroupBox.Text = "UC loadcell";
+            // 
+            // closeScaleBtn
+            // 
+            this.closeScaleBtn.Enabled = false;
+            this.closeScaleBtn.Location = new System.Drawing.Point(4, 56);
+            this.closeScaleBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.closeScaleBtn.Name = "closeScaleBtn";
+            this.closeScaleBtn.Size = new System.Drawing.Size(89, 32);
+            this.closeScaleBtn.TabIndex = 21;
+            this.closeScaleBtn.Text = "Close scale";
+            this.closeScaleBtn.UseVisualStyleBackColor = true;
+            this.closeScaleBtn.Click += new System.EventHandler(this.closeScaleBtn_click);
+            // 
+            // tarraScaleBtn
+            // 
+            this.tarraScaleBtn.Enabled = false;
+            this.tarraScaleBtn.Location = new System.Drawing.Point(97, 56);
+            this.tarraScaleBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tarraScaleBtn.Name = "tarraScaleBtn";
+            this.tarraScaleBtn.Size = new System.Drawing.Size(89, 32);
+            this.tarraScaleBtn.TabIndex = 20;
+            this.tarraScaleBtn.Text = "Tarra";
+            this.tarraScaleBtn.UseVisualStyleBackColor = true;
+            this.tarraScaleBtn.Click += new System.EventHandler(this.tarraScaleBtn_Click_1);
+            // 
+            // nullScaleBtn
+            // 
+            this.nullScaleBtn.Enabled = false;
+            this.nullScaleBtn.Location = new System.Drawing.Point(97, 20);
+            this.nullScaleBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.nullScaleBtn.Name = "nullScaleBtn";
+            this.nullScaleBtn.Size = new System.Drawing.Size(89, 32);
+            this.nullScaleBtn.TabIndex = 19;
+            this.nullScaleBtn.Text = "Null scale";
+            this.nullScaleBtn.UseVisualStyleBackColor = true;
+            this.nullScaleBtn.Click += new System.EventHandler(this.nullScaleBtn_Click_1);
+            // 
+            // openScaleBtn
+            // 
+            this.openScaleBtn.Location = new System.Drawing.Point(4, 20);
+            this.openScaleBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.openScaleBtn.Name = "openScaleBtn";
+            this.openScaleBtn.Size = new System.Drawing.Size(89, 32);
+            this.openScaleBtn.TabIndex = 18;
+            this.openScaleBtn.Text = "Open scale";
+            this.openScaleBtn.UseVisualStyleBackColor = true;
+            this.openScaleBtn.Click += new System.EventHandler(this.openScaleBtn_Click);
+            // 
+            // tarraWeightLabel
+            // 
+            this.tarraWeightLabel.AutoSize = true;
+            this.tarraWeightLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tarraWeightLabel.Location = new System.Drawing.Point(232, 59);
+            this.tarraWeightLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.tarraWeightLabel.Name = "tarraWeightLabel";
+            this.tarraWeightLabel.Size = new System.Drawing.Size(17, 24);
+            this.tarraWeightLabel.TabIndex = 17;
+            this.tarraWeightLabel.Text = "-";
+            // 
+            // weightLabel
+            // 
+            this.weightLabel.AutoSize = true;
+            this.weightLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.weightLabel.Location = new System.Drawing.Point(232, 23);
+            this.weightLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.weightLabel.Name = "weightLabel";
+            this.weightLabel.Size = new System.Drawing.Size(17, 24);
+            this.weightLabel.TabIndex = 16;
+            this.weightLabel.Text = "-";
+            // 
+            // printerGroupBox
+            // 
+            this.printerGroupBox.Controls.Add(this.feedLabelBtn);
+            this.printerGroupBox.Controls.Add(this.printTestLabelBtn);
+            this.printerGroupBox.Controls.Add(this.closePrinterBtn);
+            this.printerGroupBox.Controls.Add(this.openPrinterBtn);
+            this.printerGroupBox.Location = new System.Drawing.Point(8, 8);
+            this.printerGroupBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.printerGroupBox.Name = "printerGroupBox";
+            this.printerGroupBox.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.printerGroupBox.Size = new System.Drawing.Size(381, 107);
+            this.printerGroupBox.TabIndex = 19;
+            this.printerGroupBox.TabStop = false;
+            this.printerGroupBox.Text = "UC Printer";
+            // 
+            // feedLabelBtn
+            // 
+            this.feedLabelBtn.Enabled = false;
+            this.feedLabelBtn.Location = new System.Drawing.Point(287, 56);
+            this.feedLabelBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.feedLabelBtn.Name = "feedLabelBtn";
+            this.feedLabelBtn.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.feedLabelBtn.Size = new System.Drawing.Size(89, 32);
+            this.feedLabelBtn.TabIndex = 3;
+            this.feedLabelBtn.Text = "Feed label";
+            this.feedLabelBtn.UseVisualStyleBackColor = true;
+            this.feedLabelBtn.Click += new System.EventHandler(this.feedLabelBtn_Click);
+            // 
+            // printTestLabelBtn
+            // 
+            this.printTestLabelBtn.Enabled = false;
+            this.printTestLabelBtn.Location = new System.Drawing.Point(287, 20);
+            this.printTestLabelBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.printTestLabelBtn.Name = "printTestLabelBtn";
+            this.printTestLabelBtn.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.printTestLabelBtn.Size = new System.Drawing.Size(89, 32);
+            this.printTestLabelBtn.TabIndex = 2;
+            this.printTestLabelBtn.Text = "Print test label";
+            this.printTestLabelBtn.UseVisualStyleBackColor = true;
+            this.printTestLabelBtn.Click += new System.EventHandler(this.printTestLabelBtn_Click);
+            // 
+            // closePrinterBtn
+            // 
+            this.closePrinterBtn.Enabled = false;
+            this.closePrinterBtn.Location = new System.Drawing.Point(5, 56);
+            this.closePrinterBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.closePrinterBtn.Name = "closePrinterBtn";
+            this.closePrinterBtn.Size = new System.Drawing.Size(89, 32);
+            this.closePrinterBtn.TabIndex = 1;
+            this.closePrinterBtn.Text = "Close Printer";
+            this.closePrinterBtn.UseVisualStyleBackColor = true;
+            this.closePrinterBtn.Click += new System.EventHandler(this.closePrinterBtn_Click);
+            // 
+            // openPrinterBtn
+            // 
+            this.openPrinterBtn.Location = new System.Drawing.Point(5, 20);
+            this.openPrinterBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.openPrinterBtn.Name = "openPrinterBtn";
+            this.openPrinterBtn.Size = new System.Drawing.Size(89, 32);
+            this.openPrinterBtn.TabIndex = 0;
+            this.openPrinterBtn.Text = "Open Printer";
+            this.openPrinterBtn.UseVisualStyleBackColor = true;
+            this.openPrinterBtn.Click += new System.EventHandler(this.openPrinterBtn_Click);
+            // 
+            // MettlerScaleReader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(854, 549);
+            this.Controls.Add(this.loadcellGroupBox);
+            this.Controls.Add(this.printerGroupBox);
+            this.Controls.Add(this.eventBox);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "MettlerScaleReader";
+            this.Text = "Mettler Toledo UC Loadcell test tool";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.loadcellGroupBox.ResumeLayout(false);
+            this.loadcellGroupBox.PerformLayout();
+            this.printerGroupBox.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox eventBox;
+        private System.Windows.Forms.GroupBox loadcellGroupBox;
+        private System.Windows.Forms.Button closeScaleBtn;
+        private System.Windows.Forms.Button tarraScaleBtn;
+        private System.Windows.Forms.Button nullScaleBtn;
+        private System.Windows.Forms.Button openScaleBtn;
+        private System.Windows.Forms.Label tarraWeightLabel;
+        private System.Windows.Forms.Label weightLabel;
+        private System.Windows.Forms.GroupBox printerGroupBox;
+        private System.Windows.Forms.Button feedLabelBtn;
+        private System.Windows.Forms.Button printTestLabelBtn;
+        private System.Windows.Forms.Button closePrinterBtn;
+        private System.Windows.Forms.Button openPrinterBtn;
+    }
+
+}
---
+++ MettlerToledoLoadCellTool/MettlerScaleReader.cs
@@ -0,0 +1,304 @@
+﻿using LibUsbDotNet;
+using LibUsbDotNet.Main;
+using System;
+using System.IO;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace MettlerToledoLoadCellTool
+{
+    public partial class MettlerScaleReader : Form
+    {
+        private SerialPort _serialPort = new SerialPort("COM2", 9600, Parity.Even, 7, StopBits.Two);
+
+        private Timer _timer = new Timer();
+        private Timer _jidaTimer = new Timer();
+
+        private UcLoadcell _ucLoadcell;
+        private UsbDevice _evoLinePrinter;
+
+        public MettlerScaleReader()
+        {
+            InitializeComponent();
+        }
+
+        //
+        // Loadcell
+        //
+
+        private void sp_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string data = _serialPort.ReadLine();
+
+            this.BeginInvoke(new SetTextDeleg(si_DataReceived), new object[] { data });
+        }
+
+        private delegate void SetTextDeleg(string text);
+
+        private void si_DataReceived(string data) {
+            try
+            {
+                string nettWeight = data.Substring(21, 16);
+                string tarraWeight = data.Substring(38, 16);
+                weightLabel.Text = nettWeight;
+                tarraWeightLabel.Text = tarraWeight;
+            }
+            catch (Exception ex)
+            {
+                eventBox.Items.Insert(0, "Error: parsing weight to string");
+                eventBox.Items.Insert(0, ex.Message);
+            }
+            eventBox.Items.Insert(0, data.Trim()); 
+        }
+    
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            byte[] bytestosend = { 0x06, 0x53, 0x58, 0x49, 0x0d, 0x0a };
+            _serialPort.Write(bytestosend, 0, bytestosend.Length);
+        }
+
+        private void jidaTimer_Tick(object sender, EventArgs e)
+        {
+            _ucLoadcell.ReOpenBoardCommunication();
+        }
+
+        private void openScaleBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Open serial port
+                _serialPort.RtsEnable = false;
+                _serialPort.DtrEnable = false;
+                _serialPort.Handshake = Handshake.None;
+                _serialPort.DataReceived += new SerialDataReceivedEventHandler(sp_DataReceived);
+                _serialPort.Open();
+                eventBox.Items.Insert(0, "Open serial scale port");
+
+                // Open jida board
+                var initializeStatus = JiddaWrapper.JidaDllInitialize();
+                if (initializeStatus && JiddaWrapper.JidaDllIsAvailable())
+                {
+                    IntPtr handle = IntPtr.Zero;
+                    JiddaWrapper.JidaBoardOpenByNameA("UUP6", ref handle);
+                    _ucLoadcell = new UcLoadcell(handle);
+                    _ucLoadcell.ReOpenBoardCommunication();
+
+                    // Send inital command for loadcell
+                    byte[] bytestosend = { 0x16, 0x1B, 0x3F };
+                    _serialPort.Write(bytestosend, 0, bytestosend.Length);
+
+                    // Setup weight reading timer
+                    _timer.Interval = 500;
+                    _timer.Tick += new EventHandler(timer_Tick);
+                    _timer.Start();
+
+                    // Setup jida board reopen timer
+                    _jidaTimer.Interval = 30000;
+                    _jidaTimer.Tick += new EventHandler(timer_Tick);
+                    _jidaTimer.Start();
+
+                    nullScaleBtn.Enabled = true;
+                    tarraScaleBtn.Enabled = true;
+                    closeScaleBtn.Enabled = true;
+                }
+                else
+                {
+                    eventBox.Items.Insert(0, "No access to jida board. Open application as administrator");
+                }
+            }
+            catch (Exception ex)
+            {
+                eventBox.Items.Insert(0, $"Open scale failed {ex.Message}");
+            }
+        }
+
+        private void closeScaleBtn_click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Stop weight reading
+                _timer.Stop();
+                _timer.Dispose();                              
+
+                // Close Jida
+                JiddaWrapper.JidaDllUninitialize();
+                _jidaTimer.Stop();
+                _jidaTimer.Dispose();
+
+                // Close serial port
+                _serialPort.DataReceived -= new SerialDataReceivedEventHandler(sp_DataReceived);
+                _serialPort.Dispose();
+                _serialPort.Close();
+                eventBox.Items.Insert(0, "Close serial scale port");
+
+                nullScaleBtn.Enabled = false;
+                tarraScaleBtn.Enabled = false;
+                closeScaleBtn.Enabled = false;
+
+            }
+            catch(Exception ex)
+            {
+                eventBox.Items.Insert(0, "Error when closing scale");
+                eventBox.Items.Insert(0, ex.Message);
+            }
+        }
+
+        private void nullScaleBtn_Click_1(object sender, EventArgs e)
+        {
+            byte[] bytestosend = { 0x06, 0x5A, 0x0d, 0x0a };
+            _serialPort.Write(bytestosend, 0, bytestosend.Length);
+            eventBox.Items.Insert(0, "Set scale to null");
+        }
+
+        private void tarraScaleBtn_Click_1(object sender, EventArgs e)
+        {
+            byte[] bytestosend = { 0x06, 0x54, 0x0d, 0x0a };
+            _serialPort.Write(bytestosend, 0, bytestosend.Length);
+            eventBox.Items.Insert(0, "Tarra scale");
+        }
+
+        //
+        // Printer
+        //
+
+        private void openPrinterBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _evoLinePrinter = UsbDevice.OpenUsbDevice(new UsbDeviceFinder(0x0EB8, 0x3000));
+                _evoLinePrinter.Open();
+
+                IUsbDevice wholeUsbDevice = _evoLinePrinter as IUsbDevice;
+                if (!ReferenceEquals(wholeUsbDevice, null))
+                {
+                    wholeUsbDevice.SetConfiguration(1);
+
+                    wholeUsbDevice.ClaimInterface(0);
+                }
+
+                eventBox.Items.Add("Open usb device printer");
+
+                printTestLabelBtn.Enabled = true;
+                feedLabelBtn.Enabled = true;
+                closePrinterBtn.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                eventBox.Items.Add(ex.Message);
+                //File.AppendAllText("log.txt", ex.ToString());
+            }
+        }      
+
+        private void feedLabelBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var evoEndpointWriter = _evoLinePrinter.OpenEndpointWriter(WriteEndpointID.Ep02, EndpointType.Bulk);
+                evoEndpointWriter.Write(new byte[] { 0x01, 0x1b, 0x53 }, 1000, out int outer);
+                evoEndpointWriter.Dispose();
+                eventBox.Items.Add("Feed evo line printer");
+            }
+            catch (Exception ex)
+            {
+                eventBox.Items.Add(ex.Message);
+            }
+        }
+
+        private void closePrinterBtn_Click(object sender, EventArgs e)
+        {
+            IUsbDevice wholeUsbDevice = _evoLinePrinter as IUsbDevice;
+            if (!ReferenceEquals(wholeUsbDevice, null))
+            {
+                wholeUsbDevice.ReleaseInterface(0);
+            }
+            _evoLinePrinter.Close();
+            UsbDevice.Exit();
+
+            printTestLabelBtn.Enabled = false;
+            feedLabelBtn.Enabled = false;
+            closePrinterBtn.Enabled = false;
+        }
+
+        private void printTestLabelBtn_Click(object sender, EventArgs e)
+        {
+            int lenght;
+            byte[] data = new byte[1024];
+            ErrorCode errorCode;
+            int timeout = 500;
+
+            try
+            {
+                // Open read and writer endpoint
+                var evoEndpointWriter = _evoLinePrinter.OpenEndpointWriter(WriteEndpointID.Ep02, EndpointType.Bulk);
+                var evoEndpointReader = _evoLinePrinter.OpenEndpointReader(ReadEndpointID.Ep02);
+
+                // Create label
+                var bitmap = BitmapConverter.CreatTestBitmap(weightLabel.Text);
+                bitmap = BitmapConverter.BitmapTo1Bpp2(bitmap);
+                var command = BitmapConverter.Convert(bitmap);
+
+                var arrays = Utils.Split(command, 4000);  
+              
+                errorCode = evoEndpointWriter.Write(new byte[] { 0x01, 0x1b, 0x64, 0x31, 0x31, 0x31, 0x32 }, timeout, out lenght);
+               
+                evoEndpointReader.Read(data, timeout, out lenght);
+                eventBox.Items.Add($"Write status: {errorCode} + {Utils.ByteArrayToString(data)}");
+                
+                errorCode = evoEndpointWriter.Write(new byte[] { 0x02, 0x1b, 0x5d, 0x30, 0x31, 0x36, 0x34, 0x38 }, timeout, out lenght);
+                evoEndpointReader.Read(data, timeout, out lenght);
+                eventBox.Items.Add($"Write status: {errorCode} + {Utils.ByteArrayToString(data)}");
+                
+                errorCode = evoEndpointWriter.Write(new byte[] { 0x03, 0x1b, 0x7e }, timeout, out lenght);
+                evoEndpointReader.Read(data, timeout, out lenght);
+                eventBox.Items.Add($"Write status: {errorCode} + {Utils.ByteArrayToString(data)}");
+
+                errorCode = evoEndpointWriter.Write(new byte[] { 0x04, 0x1b, 0x5a, 0x0d, 0x0a }, timeout, out lenght);
+                evoEndpointReader.Read(data, timeout, out lenght);
+                eventBox.Items.Add($"Write status: {errorCode} + {Utils.ByteArrayToString(data)}");
+                
+                errorCode = evoEndpointWriter.Write(new byte[] { 0x05, 0x1b, 0x5a }, timeout, out lenght);
+                evoEndpointReader.Read(data, timeout, out lenght);
+                eventBox.Items.Add($"Write status: {errorCode} + {Utils.ByteArrayToString(data)}");
+                
+                errorCode = evoEndpointWriter.Write(new byte[] { 0x06, 0x1b, 0x5a }, timeout, out lenght);
+                evoEndpointReader.Read(data, timeout, out lenght);
+                eventBox.Items.Add($"Write status: {errorCode} + {Utils.ByteArrayToString(data)}");
+                
+                errorCode = evoEndpointWriter.Write(new byte[] { 0x07, 0x1b, 0xbe }, timeout, out lenght);
+                evoEndpointReader.Read(data, timeout, out lenght);
+                eventBox.Items.Add($"Write status: {errorCode} + {Utils.ByteArrayToString(data)}");
+                errorCode = evoEndpointWriter.Write(new byte[] { 0x08, 0x1b, 0x57, 0x34, 0x33, 0x32, 0x30, 0x34, 0x30, 0x30 }, timeout, out lenght);
+                
+                evoEndpointReader.Read(data, timeout, out lenght);
+                eventBox.Items.Add($"Write status: {errorCode} + {Utils.ByteArrayToString(data)}");
+
+                foreach (var array in arrays)
+                {
+                    errorCode = evoEndpointWriter.Write(array, timeout, out lenght);
+                    evoEndpointReader.Read(data, timeout, out lenght);
+                    eventBox.Items.Add($"Write label status: {errorCode} + {Utils.ByteArrayToString(data)}");
+                    if (errorCode == ErrorCode.IoTimedOut)
+                    {
+                        evoEndpointWriter.Reset();
+                    }
+                }
+                
+                // Close writer
+                evoEndpointWriter.Reset();
+                evoEndpointWriter.Abort();
+                evoEndpointWriter.Dispose();
+
+                // Close reader
+                evoEndpointReader.Reset();
+                evoEndpointReader.Abort();
+                evoEndpointReader.Dispose();
+
+                eventBox.Items.Add("Print test label evo line printer");
+            }
+            catch (Exception ex)
+            {
+                eventBox.Items.Add(ex.Message);
+            }
+        }
+    }
+}
